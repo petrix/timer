b0db6c51881043c687de00401b47699241c86eb1
petrix <yuri.petrik@gmail.com>
2018-03-16 08:30:10 +0200

    Update clockwidget.cpp

--- src/clockwidget.cpp
+++ src/clockwidget.cpp
@@ -4,7 +4,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 ClockWidget::ClockWidget(QWidget* parent) :
-    TimeWidget(Display::HrsMinSec, Qt::darkRed, parent)
+    TimeWidget(Display::HrsMinSec, Qt::darkYellow, parent)
 {
     connect(this, &ClockWidget::clicked, this, &ClockWidget::change_offset);
     connect(this, &ClockWidget::long_pressed, [](){ Clock::instance()->set_offset(0); });
