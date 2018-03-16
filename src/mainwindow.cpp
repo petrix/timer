b81d59e86f1af5d62aa3870dfa6113b90572f644
petrix <yuri.petrik@gmail.com>
2018-03-16 06:10:22 +0200

    disabled panel_2

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -12,23 +12,23 @@ MainWindow::MainWindow(QWidget* parent) :
     setupUi(this);
 
     panel_1->layout()->addWidget(_clock = new ClockWidget());
-    panel_2->layout()->addWidget(_timer = new TimerWidget());
+   // panel_2->layout()->addWidget(_timer = new TimerWidget());
     panel_3->layout()->addWidget(_count = new CountDownWidget());
 
-    connect(&_event, &Osc::Event::status_changed, [this](Osc::Event::Status status)
+   connect(&_event, &Osc::Event::status_changed, [this](Osc::Event::Status status)
     {
         switch(status)
         {
-        case Osc::Event::Started:
+    /*    case Osc::Event::Started:
             _timer->start();
             break;
 
         case Osc::Event::Stopped:
             _timer->stop();
             break;
-
+*/
         case Osc::Event::Reset:
-            _timer->reset();
+           // _timer->reset();
             _count->reset();
             reset_video_name();
             break;
