33e44582f2a5daeba5d4405105c9891093de4508
petrix <yuri.petrik@gmail.com>
2018-04-03 21:43:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+rm -rf /home/pi/timer/res/autostart.conf
+wget https://raw.githubusercontent.com/petrix/timer/master/res/autostart.conf /home/pi/timer/res/autostart.conf
+cp /home/pi/timer/res/autostart.conf /home/pi/.config/lxsession/LXDE-pi/autostart
 /home/pi/timer/timer.sh --full --blink
