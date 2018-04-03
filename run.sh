548ac3ae28ad4849e2a240195d5132ff13a3e7a4
petrix <yuri.petrik@gmail.com>
2018-04-03 18:43:13 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
-sudo su
 
-apt-get update
-
-cd /home/pi
-rm -rf oscpack
-rm -rf timer
-apt-get install -y qt5-qmake qt5-default libqt5svg5-dev git unclutter
-
-git clone https://github.com/dimitry-ishenko-casparcg/oscpack.git
-git clone https://github.com/petrix/timer.git
-cp /usr/share/plymouth/themes/pix/splash.png /usr/share/plymouth/themes/pix/splash_backup.png
-cp /home/pi/timer/casparcg2.png /usr/share/plymouth/themes/pix/splash.png
-cp /home/pi/timer/res/autostart.conf /home/pi/.config/lxsession/LXDE-pi/autostart
-
-cd /home/pi/oscpack
-
-make && make install
-
-sudo /sbin/ldconfig -v
-
-cd /home/pi/timer
-
-
-
-qmake && make
-
-cp timer timer.sh
-
-./timer.sh
+bash timer.sh --full --blink
