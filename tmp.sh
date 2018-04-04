70ec5d36e659eaffddc09cfb0a270b13fe34596b
petrix <yuri.petrik@gmail.com>
2018-04-04 08:23:11 +0300

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo (0 3  * * * root  reboot) >> /etc/crontab
-echo (0 5  * * * root  /home/pi/timer/res/time-update.sh) >> /etc/crontab
-echo (0 10  * * * root  /home/pi/timer/res/time-update.sh) >> /etc/crontab
-echo (0 15  * * * root  /home/pi/timer/res/time-update.sh) >> /etc/crontab
-echo (0 20  * * * root  /home/pi/timer/res/time-update.sh) >> /etc/crontab
+echo 0 3  * * * root  reboot >> /etc/crontab
+echo 0 5  * * * root  /home/pi/timer/res/time-update.sh >> /etc/crontab
+echo 0 10  * * * root  /home/pi/timer/res/time-update.sh >> /etc/crontab
+echo 0 15  * * * root  /home/pi/timer/res/time-update.sh >> /etc/crontab
+echo 0 20  * * * root  /home/pi/timer/res/time-update.sh >> /etc/crontab
