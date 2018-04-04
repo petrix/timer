9d1843a4ee33160507bc3220396947c5f4a5bf9d
petrix <yuri.petrik@gmail.com>
2018-04-04 03:38:23 +0300

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -2,13 +2,13 @@
 
 sudo mount /dev/mmcblk0p1 /mnt
 
-@echo off
 
-    (echo disable_overscan=0) >> /mnt/config.txt
-    (echo hdmi_group=1) >> /mnt/config.txt
-    (hdmi_hot_plug=1) >> /mnt/config.txt
-    (hdmi_mode=33) >> /mnt/config.txt
-    (disable_splash=1) >> /mnt/config.txt
-    (boot_delay=0) >> /mnt/config.txt
-    exit
+
+    echo disable_overscan=0 >> /mnt/config.txt
+    echo hdmi_group=1 >> /mnt/config.txt
+    echo hdmi_hot_plug=1 >> /mnt/config.txt
+    echo hdmi_mode=33 >> /mnt/config.txt
+    echo disable_splash=1 >> /mnt/config.txt
+    echo boot_delay=0 >> /mnt/config.txt
+  
     
