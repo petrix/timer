5bc9dde827ccd2a64aeff82fd00f8ce0242cad40
petrix <yuri.petrik@gmail.com>
2018-04-04 03:24:21 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo rm -rf /home/pi/timer/run2.sh
+sudo rm -rf /home/pi/timer/r2.sh
 cd /home/pi/timer/
-sudo wget https://github.com/petrix/timer/raw/master/run2.sh
-sudo chmod a+x run2.sh
-sudo /home/pi/timer/run2.sh
+sudo wget https://github.com/petrix/timer/raw/master/r2.sh
+sudo chmod a+x r2.sh
+sudo /home/pi/timer/r2.sh
