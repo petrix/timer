b6181f64f7cd289dcb3687e45adc1788ce1847f0
petrix <yuri.petrik@gmail.com>
2018-04-09 18:45:06 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo rm -rf /home/pi/timer/r2.sh
-cd /home/pi/timer/res/
+sudo rm -rf /opt/p3xx/timer/r2.sh
+cd /opt/p3xx/timer/res/
 sudo wget https://github.com/petrix/timer/raw/master/res/r2.sh
 sudo chmod a+x r2.sh
-sudo /home/pi/timer/res/r2.sh
+sudo /opt/p3xx/timer/res/r2.sh
