2bbb6649e4f589ab5990aef8b192dbef0d29fd16
petrix <yuri.petrik@gmail.com>
2018-04-10 22:32:15 +0300

    Update r2.sh

--- res/r2.sh
+++ res/r2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-echo "/home/pi/timer/timer.sh --full --blink" > r3.sh
+echo "#!/bin/bash" > r3.sh
+echo "/home/pi/timer/timer.sh --full --blink" >> r3.sh
 echo "/opt/p3xx/timer/timer.sh --full --blink" >> r3.sh
 echo "/opt/p3xx/cgwatch/cgwatch -f" >> r3.sh
 chmod +x r3.sh
