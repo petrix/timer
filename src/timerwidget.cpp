143e757d31f9a0c6afcbea92f49672ce11990d6c
petrix <yuri.petrik@gmail.com>
2018-04-10 20:15:07 +0300

    Update timerwidget.cpp

--- src/timerwidget.cpp
+++ src/timerwidget.cpp
@@ -7,7 +7,7 @@ const QTime midnight { 0, 0 };
 
 ////////////////////////////////////////////////////////////////////////////////
 TimerWidget::TimerWidget(QWidget* parent) :
-    TimeWidget(Display::HrsMinSec, QColor(50, 50, 70, 255), parent)
+    TimeWidget(Display::HrsMinSec, QColor(80, 80, 100), parent)
 {
     reset();
 
@@ -23,14 +23,14 @@ void TimerWidget::start()
     _epoch = Clock::instance()->datetime().addMSecs(-time().msecsSinceStartOfDay()).toMSecsSinceEpoch();
     _running = true;
 
-    set_color(QColor(100, 100, 140, 255));
+    set_color(QColor(130, 130, 160));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void TimerWidget::stop()
 {
     _running = false;
-    set_color(QColor(50, 50, 70, 255));
+    set_color(QColor(80, 80, 100));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
