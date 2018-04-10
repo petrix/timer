824cf5b08a72fa81b14bf767a934a7f81ae64acc
petrix <yuri.petrik@gmail.com>
2018-04-10 21:54:51 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo rm -rf /opt/p3xx/cgtimer/res/r2.sh
 cd /opt/p3xx/cgtimer/res/
-sudo wget https://github.com/petrix/timer/raw/master/res/r2.sh
+sudo wget https://github.com/petrix/timer/raw/cgtimer/res/r2.sh
 sudo chmod +x r2.sh
 sudo /opt/p3xx/cgtimer/res/r2.sh
