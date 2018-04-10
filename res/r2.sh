bff138c0a36a4835c7bfd7c608e8a61d26867bfc
petrix <yuri.petrik@gmail.com>
2018-04-10 23:21:48 +0300

    Update r2.sh

--- res/r2.sh
+++ res/r2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-FILE=$r4.sh     
-if [ $FILE does not exist ]; then
+     
+if [ ! -f "/opt/p3xx/cgtimer/res/r4.sh" ]; then
    #echo "File $FILE does not exist."
    
 echo "#!/bin/bash" > r3.sh
@@ -9,9 +8,10 @@ echo "/home/pi/timer/timer.sh --full --blink" >> r3.sh
 echo "/opt/p3xx/timer/timer.sh --full --blink" >> r3.sh
 echo "/opt/p3xx/cgwatch/cgwatch -f" >> r3.sh
 #echo "rm -rf r3.sh" >> r3.sh
-echo "CGTimer is next reboot" >> r4.sh
+echo "CGTimer is next reboot" > r4.sh
 chmod +x r3.sh
 bash r3.sh
+exit
 else
 rm -rf r4.sh
 echo "#!/bin/bash" > r3.sh
@@ -20,8 +20,8 @@ echo "/opt/p3xx/timer/timer.sh --full --blink" >> r3.sh
 echo "/opt/p3xx/cgtimer/cgtimer -f" >> r3.sh
 chmod +x r3.sh
 bash r3.sh
-
-fi
+exit
+fi;
 
 
 
