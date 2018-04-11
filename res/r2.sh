be6aad43ffa32a76963164527fbf305947d2d3e7
petrix <yuri.petrik@gmail.com>
2018-04-11 05:31:07 +0300

    Update r2.sh

--- res/r2.sh
+++ res/r2.sh
@@ -7,15 +7,15 @@ echo "#!/bin/bash" > r3.sh
 echo "/opt/p3xx/cgtimer/cgwatch -f" >> r3.sh
 #echo "rm -rf r3.sh" >> r3.sh
 echo "CGTimer is next reboot" > r4.sh
-chmod +x r3.sh
-bash r3.sh
+chmod +x /opt/p3xx/cgtimer/res/r3.sh
+bash /opt/p3xx/cgtimer/res/r3.sh
 exit
 else
 rm -rf r4.sh
 echo "#!/bin/bash" > r3.sh
 echo "/opt/p3xx/cgtimer/cgtimer -f" >> r3.sh
-chmod +x r3.sh
-bash r3.sh
+chmod +x /opt/p3xx/cgtimer/res/r3.sh
+bash /opt/p3xx/cgtimer/res/r3.sh
 exit
 fi;
 
