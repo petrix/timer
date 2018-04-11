f7b2f03e55d4f06bd85b8fa52b54d8b33c84f464
petrix <yuri.petrik@gmail.com>
2018-04-11 14:31:30 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-sudo rm -rf /opt/p3xx/cgtimer/res/r2.sh
-cd /opt/p3xx/cgtimer/res/
-sudo wget https://github.com/petrix/timer/raw/master/res/r2.sh
-sudo chmod +x r2.sh
-sudo /opt/p3xx/cgtimer/res/r2.sh
+     
+if [ ! -f "/opt/p3xx/cgtimer/res/r2.sh" ]; then
+   #echo "File $FILE does not exist."
+echo "CGTimer will start on next reboot" > /opt/p3xx/cgtimer/res/r2.sh
+sudo /opt/p3xx/cgtimer/cgtimer -f
+
+exit
+else
+
+rm -rf /opt/p3xx/cgtimer/res/r2.sh
+sudo /opt/p3xx/cgtimer/cgwatch -f
+
+exit
+fi;
