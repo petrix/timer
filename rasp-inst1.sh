e323125f75e73461a92f11702cb5893430708227
petrix <yuri.petrik@gmail.com>
2018-05-08 00:46:47 +0300

    Create rasp-inst1.sh

---
+++ rasp-inst1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo su
+dpkg-reconfigure tzdata
+sudo ntpd -gq
+apt-get update
+apt-get -y install python-smbus i2c-tools
+apt-get purge fake-hwclock
+mount /dev/mmcblk0p1 /mnt
+echo disable_overscan=0 >> /mnt/config.txt
+echo hdmi_group=1 >> /mnt/config.txt
+echo hdmi_hot_plug=1 >> /mnt/config.txt
+echo hdmi_mode=33 >> /mnt/config.txt
+echo disable_splash=1 >> /mnt/config.txt
+echo boot_delay=0 >> /mnt/config.txt
+
+echo elevator=deadline quiet datadev=mmcblk0p2 bootmenutimeout=0 logo.nologo console=tty3 splash loglevel=3 vt.global_cursor_default=0 plymouth.ignore-serial-consoles > /mnt/cmdline.txt
+
+echo dtparam=i2c_arm=on >> /mnt/config.txt
+echo dtoverlay=i2c-rtc,ds3231 >> /mnt/config.txt
+echo i2c-bcm2708 >> /etc/modules
+echo i2c-dev >> /etc/modules
+echo rtc-ds1307 >> /etc/modules
+
+mkdir /opt/p3xx
+cd /opt/p3xx
+
+
+wget https://github.com/petrix/timer/raw/cgwatch/res/autost-1st
+
+cp /opt/p3xx/autost-1st /home/pi/.config/lxsession/LXDE-pi/autostart
+
