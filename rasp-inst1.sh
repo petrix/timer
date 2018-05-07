5d14e8d89e8685d594dc6665525957b2784c46a3
petrix <yuri.petrik@gmail.com>
2018-05-08 00:53:25 +0300

    Update rasp-inst1.sh

--- rasp-inst1.sh
+++ rasp-inst1.sh
@@ -24,8 +24,9 @@ echo rtc-ds1307 >> /etc/modules
 mkdir /opt/p3xx
 cd /opt/p3xx
 
-
 wget https://github.com/petrix/timer/raw/master/res/autorst-1st
-
 cp /opt/p3xx/autost-1st /home/pi/.config/lxsession/LXDE-pi/autostart
 
+wget https://github.com/petrix/timer/raw/master/rasp-inst2.sh
+
+reboot
