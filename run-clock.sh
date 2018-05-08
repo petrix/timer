2b273235a628c51c5eb4e9d24fafa75b9bcfc7a7
petrix <yuri.petrik@gmail.com>
2018-05-08 04:27:39 +0300

    Update run-clock.sh

--- run-clock.sh
+++ run-clock.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 hwclock -s
-
-if [ ! -f "/opt/p3xx/r2.sh" ]; then
-
+sudo /etc/init.d/ntp stop
 sudo ntpd -gq
+sudo /etc/init.d/ntp start
+hwclock -w
+#if [ ! -f "/opt/p3xx/r2.sh" ]; then
+#sudo ntpd -gq
 #sudo hwclock -w
-cd /opt/p3xx
-sudo rm -rf /opt/p3xx/r2.sh
-sudo wget https://github.com/petrix/timer/raw/cgwatch/res/r2.sh
-sudo chmod a+x r2.sh
-sudo chmod 777 r2.sh
-sudo bash /opt/p3xx/r2.sh
+#cd /opt/p3xx
+#sudo rm -rf /opt/p3xx/r2.sh
+#sudo wget https://github.com/petrix/timer/raw/cgwatch/res/r2.sh
+#sudo chmod a+x r2.sh
+#sudo chmod 777 r2.sh
+#sudo bash /opt/p3xx/r2.sh
 
 
-exit
-else
+#exit
+#else
 
 rm -rf /opt/p3xx/r2.sh
 sudo ntpd -gq
@@ -27,5 +29,5 @@ sudo chmod a+x r2.sh
 sudo chmod 777 r2.sh
 sudo bash /opt/p3xx/r2.sh
 
-exit
-fi;
+#exit
+#fi;
