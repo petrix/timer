bc7714f26a06d7d9e99da35496db66393f20c066
petrix <yuri.petrik@gmail.com>
2018-05-08 03:45:18 +0300

    Update run-clock.sh

--- run-clock.sh
+++ run-clock.sh
@@ -6,4 +6,4 @@ sudo rm -rf /opt/p3xx/r2.sh
 sudo wget https://github.com/petrix/timer/raw/cgwatch/res/r2.sh
 sudo chmod a+x r2.sh
 sudo chmod 777 r2.sh
-sudo /opt/p3xx/r2.sh
+sudo bash /opt/p3xx/r2.sh
