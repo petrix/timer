d48b84b207bceffecb87013b152fee2256ec3674
petrix <yuri.petrik@gmail.com>
2018-05-08 14:40:00 +0300

    Update r2.sh

--- res/r2.sh
+++ res/r2.sh
@@ -14,13 +14,14 @@ git clone -b cgtimer https://github.com/petrix/timer.git /opt/p3xx/cgtimer
 cd /opt/p3xx/cgtimer
 qmake && make
 cp /opt/p3xx/cgtimer/timer /opt/p3xx/cgclock/cgtimer
+sudo /opt/p3xx/cgclock/cgtimer -f
 #sudo chmod a+x r2.sh
 #sudo chmod 777 r2.sh
 #sudo bash /opt/p3xx/r2.sh
-#exit
-#else
-
-#exit
+exit
+else
+sudo /opt/p3xx/cgclock/cgclock -f
+exit
 fi;
 
 
@@ -29,4 +30,4 @@ fi;
 #0 3 * * * reboot >/dev/null 2>&1
 #EOF
 # /home/pi/timer/timer.sh --full --blink
-sudo /opt/p3xx/cgclock/cgtimer -f
+#sudo /opt/p3xx/cgclock/cgtimer -f
