70c183f56132799103a98910cb689f12b03339c8
petrix <yuri.petrik@gmail.com>
2018-05-08 09:59:44 +0300

    Update r2.sh

--- res/r2.sh
+++ res/r2.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-crontab <<EOF
-0 3 * * * reboot >/dev/null 2>&1
-EOF
+
+
+function addtocrontab () {
+  local frequency=$1
+  local command=$2
+  local job="$frequency $command"
+  cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
+}
+addtocrontab "0 3 * * * reboot >/dev/null 2>&1"
+
+
+#crontab <<EOF
+#0 3 * * * reboot >/dev/null 2>&1
+#EOF
 # /home/pi/timer/timer.sh --full --blink
 sudo /opt/p3xx/cgclock/cgclock -f
