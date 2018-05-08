74977726f5c88e010ab7e2db5a0fbedb2545c320
petrix <yuri.petrik@gmail.com>
2018-05-08 13:32:32 +0300

    Update r2.sh

--- res/r2.sh
+++ res/r2.sh
@@ -8,6 +8,21 @@ function addtocrontab () {
   cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
 }
 addtocrontab "0 3,11 * * * reboot >/dev/null 2>&1"
+if [ ! -f "/opt/p3xx/cgtimer" ]; then
+git clone -b cgtimer https://github.com/petrix/timer.git /opt/p3xx/cgtimer
+
+cd /opt/p3xx/cgtimer
+qmake && make
+cp /opt/p3xx/cgclock/cgtimer /opt/p3xx/cgtimer/cgtimer
+#sudo chmod a+x r2.sh
+#sudo chmod 777 r2.sh
+#sudo bash /opt/p3xx/r2.sh
+exit
+else
+
+exit
+fi;
+
 
 
 #crontab <<EOF
